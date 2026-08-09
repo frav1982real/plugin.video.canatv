d4b04d6d1b0ea777c2924b09af75a0d54715f304
FraV1982 <consulenza.vangi@outlook.com>
2026-08-09 19:17:50 +0000

    test

--- default.py
+++ default.py
@@ -1,5 +1,22 @@
 # -*- coding: utf-8 -*-
-from resources.lib.router import run
+import os
+import sys
+import re
+import xbmc
+import xbmcaddon
+import xbmcgui
+import logging
 
-if __name__ == '__main__':
-    run()
+addon_id = "plugin.video.canatv"
+xbmcaddon.Addon(id=addon_id).getSetting("debug")
+
+def test():
+    info = xbmcaddon.Addon(id=addon_id).getAddonInfo()
+
+    def log():
+        dialog = xbmcgui.Dialog()
+        dialog.ok(info)
+
+    log()
+
+test()
